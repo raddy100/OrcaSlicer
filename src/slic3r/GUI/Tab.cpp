bambulab/BambuStudio

FIX: do not show multi extruder params in single extruder
jira: STUDIO-12679

Signed-off-by: xun.zhang <xun.zhang@bambulab.com>
Change-Id: I3bba9ac0bddf0f03eb79b2eaf71e1dee8e2741a7

src/slic3r/GUI/Tab.cpp
@@ -3629,6 +3629,7 @@ void TabFilament::toggle_options()
             m_preset_bundle->printers.get_edited_preset().is_bbl_vendor_preset(
                 m_preset_bundle);
     }
+    bool is_multi_extruder = m_preset_bundle->printers.get_edited_preset().config.option<ConfigOptionFloatsNullable>("nozzle_diameter")->size() > 1;
 
     if (m_active_page->title() == "Cooling")
     {
@@ -3667,9 +3668,10 @@ void TabFilament::toggle_options()
             toggle_line(el, is_BBL_printer);
     }
 
-    if(m_active_page->title() == "Multi Filament"){
+    if (m_active_page->title() == "Multi Filament") {
         const int extruder_idx = m_variant_combo->GetSelection();
-        toggle_line("retraction_distances_when_ec", m_config->opt_bool_nullable("long_retractions_when_ec", extruder_idx), 256 + extruder_idx);
+        toggle_line("long_retractions_when_ec", is_multi_extruder && is_BBL_printer, 256 + extruder_idx);
+        toggle_line("retraction_distances_when_ec", is_multi_extruder && is_BBL_printer && m_config->opt_bool_nullable("long_retractions_when_ec", extruder_idx), 256 + extruder_idx);
     }
 
     if (m_active_page->title() == "Setting Overrides")

raddy100/OrcaSlicer

(cherry picked from commit b4f2ff2bdc47d9295135a668787dcd0349e9694d)

src/slic3r/GUI/Tab.cpp
@@ -3962,6 +3962,7 @@ void TabFilament::toggle_options()
       is_BBL_printer =
           wxGetApp().preset_bundle->is_bbl_vendor();
     }
+    bool is_multi_extruder = m_preset_bundle->printers.get_edited_preset().config.option<ConfigOptionFloats>("nozzle_diameter")->size() > 1;
 
     auto cfg = m_preset_bundle->printers.get_edited_preset().config;
     if (m_active_page->title() == L("Cooling")) {
@@ -4036,7 +4037,8 @@ void TabFilament::toggle_options()
         toggle_option("filament_multitool_ramming_flow", multitool_ramming);
 
         const int extruder_idx = 0; // m_variant_combo->GetSelection(); // TODO: Orca hack
-        toggle_line("retraction_distances_when_ec", m_config->opt_bool("long_retractions_when_ec", extruder_idx), 256 + extruder_idx);
+        toggle_line("long_retractions_when_ec", is_multi_extruder && is_BBL_printer, 256 + extruder_idx);
+        toggle_line("retraction_distances_when_ec", is_multi_extruder && is_BBL_printer && m_config->opt_bool("long_retractions_when_ec", extruder_idx), 256 + extruder_idx);
     }
 }
 